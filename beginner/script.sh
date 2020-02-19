b4503e333ef62753d7636060fc5c863494a028e4
Mariusz Kolodziej <mariusz.kolodziej@outlook.com>
2020-02-19 10:56:43 -0500

    3 - Add parameters

--- beginner/script.sh
+++ beginner/script.sh
@@ -12,12 +12,34 @@ az group create \
 templateFile="template.json"
 
 az group deployment create \
-  --name addstorage \
+  --name addnameparameter \
   --resource-group $myResoruceGroup \
-  --template-file $templateFile
+  --template-file $templateFile \
+  --parameters storageName=storageaccount2mk
+
+# Deploy the second template with SKU parameter default
+templateFile="template-two.json"
+
+az group deployment create \
+  --name addskuparameter \
+  --resource-group $myResoruceGroup \
+  --template-file $templateFile \
+  --parameters storageName=storageaccount2mk
+
+# Deploy the second template with SKU parameter updated SKU to GRS
+az group deployment create \
+  --name addskuparameter \
+  --resource-group $myResoruceGroup \
+  --template-file $templateFile \
+  --parameters storageSKU=Standard_GRS storageName=storageaccount2mk
 
 # Verify the deployment in the portal
 # Az docs - https://docs.microsoft.com/en-us/cli/azure/group/deployment?view=azure-cli-latest
 
 az group deployment --help
 az group deployment create --help
+
+# Clean up resoruces group
+az group delete \
+  --resource-group $myResoruceGroup \
+  --yes
