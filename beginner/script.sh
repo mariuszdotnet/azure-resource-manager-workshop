e877d0e8aed6d57affd618920eea0505666f9728
Mariusz Kolodziej <mariusz.kolodziej@outlook.com>
2020-02-19 14:43:02 -0500

    7 -  Use exported template

--- beginner/script.sh
+++ beginner/script.sh
@@ -12,7 +12,7 @@ az group create \
 templateFile="template.json"
 
 az group deployment create \
-  --name addoutputs \
+  --name addappserviceplan \
   --resource-group $myResoruceGroup \
   --template-file $templateFile \
   --parameters storagePrefix=store storageSKU=Standard_LRS
