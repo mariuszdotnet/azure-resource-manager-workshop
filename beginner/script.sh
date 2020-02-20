6cca098f9cdeb0e872a88d1546e5dc73cef311fe
Mariusz Kolodziej <mariusz.kolodziej@outlook.com>
2020-02-19 21:32:58 -0500

    10 - Use parameter file

--- beginner/script.sh
+++ beginner/script.sh
@@ -3,20 +3,33 @@
 myResoruceGroup="my-resource-group-rg"
 region="canada central"
 
-# Create Empty Resoruce Group
-az group create \
-  --name $myResoruceGroup \
-  --location "$region"
-
 # Deploy the Empty Template
 templateFile="template.json"
 
+# Create the resoruce group and resoruces for DEV
+az group create \
+  --name myResourceGroupDev \
+  --location "$region"
+
 az group deployment create \
   --handle-extended-json-format \
-  --name addtags \
-  --resource-group $myResoruceGroup \
+  --name devenvironment  \
+  --resource-group myResourceGroupDev \
   --template-file $templateFile \
-  --parameters storagePrefix=store storageSKU=Standard_LRS webAppName=demoapp
+  --parameters azuredeploy.parameters.dev.json
+
+# Create the resoruce group and resoruces for PROD
+az group create \
+  --name myResourceGroupProd \
+  --location "$region"
+
+az group deployment create \
+  --handle-extended-json-format \
+  --name prodenvironment  \
+  --resource-group myResourceGroupProd \
+  --template-file $templateFile \
+  --parameters azuredeploy.parameters.prod.json
+
 
 # Verify the deployment in the portal
 # Az docs - https://docs.microsoft.com/en-us/cli/azure/group/deployment?view=azure-cli-latest
